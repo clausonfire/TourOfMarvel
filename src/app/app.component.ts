0763832695b1fe790778d71f37b7f05911a0c238
Claudia <claudia2daw@gmail.com>
2023-02-21 23:23:13 +0100

    marvel style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tour of Heroes';
 }
+
+// que es la inyeccion de dependencias?
+
+
+// que es un servicio?
+// es un proovedor de datos que puede inyectar otras dependencias de la aplicación, 
+// ya sean otros servicios como el de Http
+
+// que es un observable?
+// el servicio consigue unos recursos y el observable los provee
+
+// que hace el suscribe?
+// es un equivalete al .then
+
+// que hace el pipe? 
+// herramienta para transformar los datos a la hora de mostrarlos
